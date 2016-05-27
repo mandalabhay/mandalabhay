ce82e7b216475ac9347f15ee525591bbd3b6e028
mandalabhay <abhaymandal@gmail.com>
2016-05-27 22:54:40 +0530

    Model Demo Homepage
    Model Demo

---
+++ Model/js/master.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+	// Homepage crousle continuse rotation
+	if($('#myCarousel').length>0){
+		$('#myCarousel').carousel({
+		  interval: 3000,
+		  cycle: true
+		}); 
+	}
+
+
+// end of ready function
+})
